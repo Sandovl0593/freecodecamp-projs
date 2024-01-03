df5e7dc3c8ffe197bddb2320880a01a322c83f32
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2024-01-03 17:52:20 -0500

    Complete 2/5 partial

--- MarkdownPreviewer/App.jsx
+++ MarkdownPreviewer/App.jsx
@@ -1,9 +1,49 @@
+import React from "react";
+import MarkdownPreview from "@uiw/react-markdown-preview";
+import markdownText from "./markdown.js";
 import "./App.css";
+
+class Previewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: markdownText,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState((state) => ({
+      input: e.target.value,
+    }));
+  }
+
+  render() {
+    return (
+      <div style={{ paddingBottom: "3em" }}>
+        <div id="editor-box">
+          <h3>Text</h3>
+          <textarea id="editor" onChange={this.handleChange}>
+            Your Markdown Text
+          </textarea>
+        </div>
+        <br />
+        <div>
+          <h3>Preview</h3>
+          <div id="preview">
+            <MarkdownPreview source={this.state.input} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <>
-      <h1 className="text-secondary">Vite + React</h1>
+      <h1>Markdown Previewer</h1>
+      <Previewer />
     </>
   );
 }
